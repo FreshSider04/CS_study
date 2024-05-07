32e280ec43ed2b1e41767c0544c4960b1950d0d3
thelittleho <jyh20041206@gmail.com>
2024-05-07 16:49:57 +0900

    FastAPI base

--- Sophomore/Data Structure/University_Homework/LinkedList.c
+++ Sophomore/Data Structure/University_Homework/LinkedList.c
@@ -1,98 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct listnode *listPointer;
-typedef struct listnode
-{
-    int data ;
-    listPointer link;
-} listNode;
+typedef int element;
+typedef struct ListNode{
+    element data;
+    struct ListNode *link;
+} ListNode;
 
-// 두 개의 노드를 가진 연결 리스트 생성 함수
-listPointer create2()
-{
 
-    listPointer first = (listNode *)malloc(sizeof(listNode));
-    listPointer second = (listNode *)malloc(sizeof(listNode));
 
-    second->link = NULL;
-    second->data = 20;
-
-    first->data = 10;
-    first->link = second;
-
-    return first;
+ListNode* firstInsert(ListNode *head, element value){
+    ListNode *p=(ListNode *)malloc(sizeof(ListNode));
+    p->data=value;
+    p->link=head;
+    head=p;
+    return head;
 }
 
-// 리스트 삭제
-void delete(listPointer *first, listPointer trail, listPointer x)
-{
-    if (trail)
-        trail->link = x->link;
-    else
-        *first = (*first)->link;
-    free(x);
+ListNode* insert(ListNode *head,ListNode* pre, element value){
+    ListNode *p=(ListNode *)malloc(sizeof(ListNode));
+    p->data=value;
+    p->link=pre->link;
+    pre->link=p;
+    return head;
 }
 
-// 리스트 출력
-void printList(listPointer first)
-{
-    for (; first; first = first->link)
-        printf("%4d ->", first->data);
-
-    putchar('\n') ;
+ListNode* delete(ListNode *head,ListNode *pre){
+    ListNode *removed;
+    removed=pre->link;
+    pre->link=removed->link;
+    free(removed);
+    return head;
 }
 
-// 리스트 삽입
-void insert(listPointer *first, listPointer x, int data)
-{
-    listPointer temp = (listNode *)malloc(sizeof(listNode));
-
-    temp->data = data;
-
-    if (*first) 
-    {
-        temp->link = x->link;
-        x->link = temp;
+void printL(ListNode *head){
+    for (ListNode *p=head;p!=NULL;p=p->link){
+        printf("%d->",p->data);
     }
+    printf("NULL\n");
+}
+int main() {
+    ListNode *head=NULL;
 
-    else
-    {
-        temp->link = NULL;
-        *first = temp;
+    // 1 ~ 5까지 무작위로 삽입
+    for(int i = 1; i <= 5; i++){
+        head = firstInsert(head,i);
+        printL(head);
+        printf("\n");
     }
-}
-
-listPointer movex(listPointer first)
-{
-    listPointer pointer ;
-    for (; first; first = first->link) 
-        pointer = first ;       
-    return pointer;
-}
-
-int main()
-{
-    create2(); 
-    
-    listPointer first = NULL; 
-    listPointer x = NULL ;
-
-    first =  create2() ;
-    printList(first)    ;
-
-    x = movex(first); // x를 끝으로 이동
-
-    insert(&first, x ,50) ;
-    printList(first)    ;
-    
-    x = movex(first); // x를 끝으로 이동
-    insert(&first, x ,30) ;
-    printList(first)    ;
-      
-    x = movex(first); // x를 끝으로 이동
-    insert(&first, x ,100) ;
-    printList(first)    ;
 
     return 0;
 }
